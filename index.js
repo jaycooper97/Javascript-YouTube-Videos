6cbfea16f4fd60194f30d33931569544561e113a
jay cooper <15jacooper@gmail.com>
2023-08-02 21:38:54 -0400

    new push

---
+++ index.js
@@ -0,0 +1,99 @@
+alert("Hello World"); 
+//alert("Hi again");
+
+//make the image invisible
+//move the image from top to bottom
+//make the image visible
+
+var myVariableJay;
+myVariableJay = 10;
+
+var myVar2 = "Hello";
+myVar2 = 'Hello again'
+myVar2 = 5;
+
+var customerAge = 32;
+customerAge = 32;
+
+
+var myVar3 = 40;
+
+var myVar = 5;
+5 + 5;
+
+myVar = myVar + 10
+myVar = 15
+
+myVar - 3
+myVar = 12
+
+myVar = myVar - 5
+myVar = 10
+
+
+10 * 10;
+20 / 10;
+
+
+myVar = myVar / 2;
+myVar = 5
+
+
+
+
+5 + 'Hello';
+'5Hello'
+
+100 + 'Hello';
+'100Hello';
+
+'hello' + 'david';
+'hellodavid'
+
+'hello' + ' david';
+'hello david'
+
+
+5 * 'hello';
+NaN
+
+//Shorthand Methods
+
+var myVar = 10;
+myVar = myVar + 5;
+myVar = 15;
+               OR
+myVar += 5;
+myVar = 20;
+
+
+myVar -= 10;
+myVar = 10;
+
+myvar /= 2;
+myVar = 5;
+
+myVar *= 5;
+myVar = 25;
+
+myVar += 1;
+myVar = 26;
+             OR
+myVar++;
+myVar = 27;
+
+
+//Logging to Console/Document
+
+var myVar = Jay;
+document.write(myVar);
+console.log(myVar);
+
+
+document.write(10);
+console.log(50);
+
+console.log(20 * 5);
+console.log(20 / 2);
+
+
